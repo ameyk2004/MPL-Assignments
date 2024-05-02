ab5ca61dfae47229f514dc922ae28940e52a711d
Amey Kulkarni <148298416+ameyk2004@users.noreply.github.com>
2024-05-02 09:35:01 +0530

    Assignment 2

--- Assignment_02/assn2.asm
+++ Assignment_02/assn2.asm
@@ -1,7 +1,78 @@
-section .data
-    msg1 db "Enter a String : "
-    msglen1 equ-$ msg1
+%macro disp 2
+mov rax, 1
+mov rdi, 1
+mov rsi, %1
+mov rdx, %2
+syscall
+%endmacro
 
-    msg2 db "Length = "
-    msglen2 equ-$ msg2
+%macro read 2
+mov rax, 0
+mov rdi, 0
+mov rsi, %1
+mov rdx, %2
+syscall
+%endmacro
+
+section .data
+    msg1 db 10,13,"Enter a String : "
+    msg1len equ $-msg1
+
+    msg2 db 10,13,"Length : "
+    msg2len equ $-msg2
+
+    count db 16
+
+section .bss
+    str1 resb 20
+    resultarr resb 16
+    
+
+section .text
+    global _start
+    _start:
+
+    disp msg1, msg1len
+    read str1, 20
+
+    ;rax madhe Length store zhaliye 
+    ;hex form madhe
+    ;ata aplyala hex to ascii hex karaychay
+
+    ;rax ----> QUAD WORD
+    
+mov rbp, resultarr
+move byte[count], 16 
+
+up:    ;rax -> 2038262038262038
+rol rax, 04 
+mov bl, al
+and bl, 0F
+cmp bl, 09
+jle next1
+add bl, 07
+
+next1:
+add bl, 30
+move [rbp], bl
+inc rbp
+dec byte[count]
+jnz up
+
+
+disp resultarr, 16
+
+mov rax,60
+mov rdi, 0
+syscall
+
+
+
+
+
+
+
+
+
+
 
